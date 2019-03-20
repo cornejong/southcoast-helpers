1a867c30f3d6055ac938d59ec3b802bb6bbac0ac
Corné de Jong <corne.dejong@me.com>
2019-03-20 15:16:18 +0100

    Added Fridge storage

---
+++ src/helpers/Fridge.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SouthCoast\Helpers;
+
+class Fridge
+{
+
+    private static $fridge;
+
+    public static function get(string $query, bool $subtract_query = true, bool $do_rebuild = true)
+    {
+        return ArrayHelper::get($query, self::$fridge, $subtract_query, $do_rebuild)
+    }
+
+    public static function set(string $query, $value)
+    {
+        self::$fridge = ArrayHelper::add($query, $value, self::$fridge);
+    }
+
+    public static function clear()
+    {
+        self::$fridge = [];
+    }
+
+    public static function export()
+    {
+        return ArrayHelper::sanitize(self::$fridge);
+    }
+
+}
