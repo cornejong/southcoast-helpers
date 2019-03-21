9fcfdf05ca9513d587108b3efd3ba3fd63a22735
Corné de Jong <corne.dejong@me.com>
2019-03-21 16:21:01 +0100

    Update Fridge

--- src/helpers/Fridge.php
+++ src/helpers/Fridge.php
@@ -9,12 +9,17 @@ class Fridge
 
     public static function get(string $query, bool $subtract_query = true, bool $do_rebuild = true)
     {
-        return ArrayHelper::get($query, self::$fridge, $subtract_query, $do_rebuild)
+        return ArrayHelper::get($query, self::$fridge, $subtract_query, $do_rebuild);
     }
 
     public static function set(string $query, $value)
     {
         self::$fridge = ArrayHelper::add($query, $value, self::$fridge);
+    }
+
+    public static function has(string $query)
+    {
+        return ArrayHelper::get($query, self::$fridge) !== null ? true : false;
     }
 
     public static function clear()
