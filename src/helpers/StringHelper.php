fe2ea9b3320609fbf96680e48ac19b5c406a7b35
Corné de Jong <corne.dejong@me.com>
2019-04-19 22:04:20 +0100

    Added clean method

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -6,22 +6,38 @@ use \Error;
 
 class StringHelper
 {
-
+    /**
+     * @param string $needle
+     * @param string $string
+     */
     public static function contains(string $needle, string $string)
     {
         return strpos($string, $needle) !== false ? true : false;
     }
 
+    /**
+     * @param string $needle
+     * @param string $string
+     * @return mixed
+     */
     public static function startsWith(string $needle, string $string)
     {
         return $needle === substr($string, 0, strlen($needle)) ? true : false;
     }
 
+    /**
+     * @param string $needle
+     * @param string $string
+     * @return mixed
+     */
     public static function endsWith(string $needle, string $string)
     {
         return $needle === substr($string, -strlen($needle), strlen($needle)) ? true : false;
     }
 
+    /**
+     * @param $data
+     */
     public function stringify($data)
     {
         switch (gettype($data)) {
@@ -53,9 +69,47 @@ class StringHelper
         }
     }
 
+    /**
+     * @param string $string
+     * @return mixed
+     */
     public static function explodeCamelCase(string $string): array
     {
         preg_match_all('/((?:^|[A-Z])[a-z]+)/', $string, $matches);
         return $matches[0];
+    }
+
+    /**
+     * removes all special characters from a given string
+     *
+     * @param string $text
+     * @return string
+     */
+    public static function clean(string $text): string
+    {
+        $utf8 = [
+            '/[áàâãªä]/u' => 'a',
+            '/[ÁÀÂÃÄ]/u' => 'A',
+            '/[ÍÌÎÏ]/u' => 'I',
+            '/[íìîï]/u' => 'i',
+            '/[éèêë]/u' => 'e',
+            '/[ÉÈÊË]/u' => 'E',
+            '/[óòôõºö]/u' => 'o',
+            '/[ÓÒÔÕÖ]/u' => 'O',
+            '/[úùûü]/u' => 'u',
+            '/[ÚÙÛÜ]/u' => 'U',
+            '/ç/' => 'c',
+            '/Ç/' => 'C',
+            '/ñ/' => 'n',
+            '/Ñ/' => 'N',
+            '/–/' => '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u' => ' ', // Literally a single quote
+            '/[“”«»„]/u' => ' ', // Double quote
+            '/ /' => ' ', // nonbreaking space (equiv. to 0x160)
+        ];
+
+        $result = preg_replace(array_keys($utf8), array_values($utf8), $text);
+
+        return $result ?? $text;
     }
 }
